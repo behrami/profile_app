da9fb1c4a80d21cfb0defa6764cf346fd73a2beb
behrami <behram1993@gmail.com>
2018-07-05 21:26:08 -0400

    basic routes for display

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # routes to display info
+  resources :users only: %i[show] do
+    resources :blogs only: %i[index show]
+    resource :resume only: %i[show] do
+      resources :educations only: %i[index]
+      resources :experiences only: %i[index]
+      resources :projects only: %i[index]
+      resources :skills only: %i[index]
+    end
+  end
+
+  # routes to create and edit stuff
+  
 end
